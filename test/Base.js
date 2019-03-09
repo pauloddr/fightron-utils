05f5130d0dc9b8c328d88bd190c7e8540be76237
@pauloddr <git@pauloddr.com>
2019-03-09 23:35:31 +0000

    move Base/Counter

---
+++ test/Base.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import { Base } from '../Base'
+
+describe('Base', function () {
+  before(function () {
+    this.instance = new Base()
+  })
+
+  it('is not free', function () {
+    expect(this.instance._free).to.equal(false)
+  })
+
+  describe('#free', function () {
+    before(function () {
+      this.instance.free()
+    })
+
+    it('sets instance free', function () {
+      expect(this.instance._free).to.equal(true)
+    })
+  })
+})
---
+++ test/Counter.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import { Counter } from '../Counter'
+
+describe('Counter', function () {
+  // REPLACE WITH REAL TESTS
+  it('exists', function () {
+    expect(Counter).to.exist()
+  })
+})
